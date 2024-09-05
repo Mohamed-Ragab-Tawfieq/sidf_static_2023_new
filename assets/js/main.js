7c0cf9c359abe47d83fbf0132de37f453251bc2e
Mohamed Ragab <mohamed.tawfieq@asgatech.com>
2024-09-05 15:18:26 +0300

    counters

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     setTimeout(() => {
         loader?.classList.add('d-none')
-    }, 11000);
+    }, 100);
 })
 
 /*---------------- Home start ----------------*/
@@ -153,7 +153,22 @@ $(document).ready(function () {
 /*** counters ***/
 $(document).ready(function () {
     if ($('[data-purecounter-end]').length) {
-        new PureCounter();
+        new PureCounter({
+            // Setting that can't' be overriden on pre-element
+            selector: '.purecounter', // HTML query selector for spesific element
+
+            // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+            start: 0, // Starting number [uint]
+            duration: 1, // The time in seconds for the animation to complete [seconds]
+            delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+            once: true, // Counting at once or recount when the element in view [boolean]
+            pulse: false, // Repeat count for certain time [boolean:false|seconds]
+            legacy: true, // If this is true it will use the scroll event listener on browsers
+            filesizing: false, // This will enable/disable File Size format [boolean]
+            formater: 'us-US', // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+            separator: true, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+        });
+
     }
 })
 
